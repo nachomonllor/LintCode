85d2243bffe3b18a4387a51303941e69413e8ddb
nachomonllor <nachomonllorc@gmail.com>
2019-06-04 14:24:41 -0300

    agrego archivos

---
+++ deleteNode.java
@@ -0,0 +1,19 @@
+﻿public ListNode deleteNode(ListNode head, int n, int m) {
+        // Write your code here
+        
+         ListNode ans = new ListNode(head.val);
+        int i =0;
+        for(ListNode nodo = head; nodo != null; nodo = nodo.next) {
+            if(i>=n && i <= m) {
+                ans.next = null;
+                //head = head.next;
+            }
+            else{
+                ans.next = nodo;
+            }
+            i++;
+        }
+        
+        return ans;
+        
+    }
---
+++ rotate string.java
@@ -0,0 +1,21 @@
+﻿ public static void  rotateString(char[] str, int offset)
+    { 
+        
+        if(str.length ==0)return;
+        
+        // write your code here
+        offset = offset % str.length;
+        
+       if(offset > str.length ) {
+            return;
+        }
+        
+        //usando string auxiliar
+        String s = new String(str);
+        String x = s.substring(s.length() - offset)
+            + s.substring(0, s.length() - offset);
+        char[] ch = x.toCharArray();
+        System.arraycopy(ch, 0, str, 0, str.length);
+
+    }
+    
