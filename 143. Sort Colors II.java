4ac25338d3556c753bffdff16c2a6f98a234fc44
nachomonllor <nachomonllorc@gmail.com>
2019-06-10 12:40:26 -0300

    agrego archivos

---
+++ 143. Sort Colors II.java
@@ -0,0 +1,21 @@
+﻿public static void sortColors2(int[] colors, int k)
+        {
+            // write your code here
+            int[] count = new int[k + 1];
+
+            for (int i = 0; i < colors.length; i++)
+            {
+                count[colors[i]]++;
+            }
+
+            int indice = 0;
+            for (int i = 0; i <= k; i++)
+            {
+                for (int j = 0; j < count[i]; j++)
+                {
+                    colors[indice++] = i;
+                }
+
+            }
+
+        }
